edb523e360c376cdfc0d51aad7e19f0454ca6aef
Jay <jjs_0630@naver.com>
2024-03-16 16:22:24 +0900

    second commit for check

--- pointer-test.c
+++ pointer-test.c
@@ -5,6 +5,8 @@ int main(){
 
     i = 10;     // i에 10 저장
 
+    char hi='?'; // for git track
+
     printf("value of i = %d\n", i);     // 10이 저장된 i의 값 출력
     printf("address of i = %p\n", &i);  // & 을 사용함으로서 i의 주소값을 출력
     printf("value of p = %p\n", p);     // 포인터 변수p의 값을 출력
