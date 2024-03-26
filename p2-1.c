1c6462d8347608f88701d4704001a4aa8d51da59
Jay <jjs_0630@naver.com>
2024-03-26 18:07:43 +0900

    week3

---
+++ p2-1.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+
+float sum1(float list[], int);      // 주소를 배열 형태로 받기
+float sum2(float *list, int);       // 주소를 포인터 형태로 받기
+float sum3(int n, float *list);     // 주소를 포인터 형태로 받기
+float input[MAX_SIZE], answer;      // 전역 변수와 배열 선언
+int i;
+
+void main(void)
+{
+    for(i=0; i < MAX_SIZE; i++)
+        input[i] = i;   // 배열에 0부터 99까지 값 입력
+    /* for checking call by reference */
+    printf("--------------------------------------\n");
+    printf(" sum1(input, MAX_SIZE) \n");
+    printf("--------------------------------------\n");
+    printf("input \t= %p\n", input);            // 배열의 주소 출력
+    answer = sum1(input, MAX_SIZE);             // 배열의 이름을 주소로서 넘겨줌
+    printf("The sum is: %f\n\n", answer);       // 베열의 합계 출력
+    printf("--------------------------------------\n");
+    printf(" sum2(input, MAX_SIZE) \n");
+    printf("--------------------------------------\n");
+    printf("input \t= %p\n", input);            // 배열의 주소 출력
+    answer = sum2(input, MAX_SIZE);             // 배열의 이름을 주소로서 넘겨줌
+    printf("The sum is: %f\n\n", answer);       // 베열의 합계 출력
+    printf("--------------------------------------\n");
+    printf(" sum3(MAX_SIZE, input) \n");
+    printf("--------------------------------------\n");
+    printf("input \t= %p\n", input);            // 배열의 주소 출력
+    answer = sum3(MAX_SIZE, input);             // 배열의 이름을 주소로서 넘겨줌
+    printf("The sum is: %f\n\n", answer);       // 베열의 합계 출력
+}
+
+float sum1(float list[], int n) {               // 배열로서 주소를 받음
+    printf("list\t= %p\n", list);               // 받은 배열의 주소값 출력
+    printf("&list\t= %p\n\n", &list);           // 함수에서 만들어진 list배열의 주소값 출력
+    int i;
+    float tempsum = 0;
+    for(i = 0;i < n;i++)
+        tempsum += list[i];
+    return tempsum;
+}
+float sum2(float *list, int n) {                // 포인터로서 주소를 받음
+    printf("list\t= %p\n", list);               // 받은 배열의 주소값 출력
+    printf("&list\t= %p\n\n", &list);           // 함수에서 만들어진 list배열의 주소값 출력
+    int i;
+    float tempsum = 0;
+    for(i = 0;i < n;i++)
+        tempsum += *(list + i);                 // 포인터에 일정한 값을 더하여 다음 배열을 가르켜 그 값에 접근한다
+    return tempsum;
+}
+/* stack variable reuse test */
+float sum3(int n, float *list) {                // 포인터로서 주소를 받음
+    printf("list\t= %p\n", list);               // 받은 배열의 주소값 출력
+    printf("&list\t= %p\n\n", &list);           // 함수에서 만들어진 list배열의 주소값 출력
+    int i;
+    float tempsum = 0;
+    for(i = 0;i < n;i++)
+        tempsum += *(list +i);
+    return tempsum;
+}
---
+++ p2-2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void print_one(int *ptr, int rows);
+
+int main(){
+    int one[] = {0, 1, 2, 3, 4};            // 크기 5인 배열 선언
+    printf("one = %p\n", one);              // 배열의 이름으로서 포인터 이고 배열의 시작 주소값 출력
+    printf("&one = %p\n", &one);            // 배열의 이름으로서 포인터 이고 포인터의 주소값 출력
+    printf("&one[0] = %p\n", &one[0]);      // 배열의 이름으로서 포인터 이고 배열의 첫번째 주소값 출력
+    printf("\n");
+    printf("------------------------------------\n");
+    printf(" print_one(&one[0], 5) \n");
+    printf("------------------------------------\n");
+    print_one(&one[0], 5);                  // 배열의 첫번째 주소값을 넘김
+    printf("------------------------------------\n");
+    printf(" print_one(one, 5) \n");
+    printf("------------------------------------\n");
+    print_one(one, 5);                      // 배열의 이름, 즉 배열의 주소값을 넘김
+    return 0;
+}
+
+void print_one(int *ptr, int rows){     // 포인터로서 배열의 주소값을 받음
+/* print out a one-dimensional array using a pointer */
+    int i;
+    printf ("Address \t Contents\n");
+    for (i = 0; i < rows; i++)
+        printf("%p \t %5d\n", ptr + i, *(ptr + i));     // 포인터에 일정한 수를 더함으로서 배열에 하나씩 접근하여 주소와 값을 출력하고 있다
+    printf("\n");
+}
